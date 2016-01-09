d3cff0615ea68563dc6491f191afcc30517dcbfb
Roi Tiger <roi@fb.com>
2016-01-09 21:24:11 +0200

    playlists

--- MusixAPI.php
+++ MusixAPI.php
@@ -244,7 +244,7 @@ private function mmdFromTracks($tracks) {
 
     private function musixMyPlaylists($user_guid)
     {
-      $url = 'http://musix-simplay.s3-eu-west-1.amazonaws.com/Customers/13/Users/' . $user_guid . '/Playlists.json';
+      $url = 'http://musix-simplay.s3-eu-west-1.amazonaws.com/Customers/13/Users/' . strtoupper($user_guid) . '/Playlists.json';
       error_log($url);
       $resp = Requests::get($url);
 
